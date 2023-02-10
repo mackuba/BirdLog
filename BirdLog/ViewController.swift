bd0061513418061697633cc95cc4c2acad268ff5
Kuba Suder <jakub.suder@gmail.com>
2023-02-10 15:03:49 +0200

    created empty app project from the template

---
+++ BirdLog/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BirdLog
+//
+//  Created by Kuba Suder on 10/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
