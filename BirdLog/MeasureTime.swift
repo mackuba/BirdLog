b623e6d9943092d5ef6f4ceb3d2948f347d4895a
Kuba Suder <jakub.suder@gmail.com>
2023-04-14 01:35:29 +0200

    added util function for measuring time

---
+++ BirdLog/MeasureTime.swift
@@ -0,0 +1,23 @@
+//
+//  MeasureTime.swift
+//  BirdLog
+//
+//  Created by Kuba Suder on 13/04/2023.
+//  Licensed under Mozilla Public License 2.0
+//
+
+import Foundation
+import os
+
+func measureTime<ReturnType>(_ block: () throws -> ReturnType) rethrows -> ReturnType {
+    let start = DispatchTime.now()
+    let returnValue = try block()
+    let end = DispatchTime.now()
+
+    let nano = end.uptimeNanoseconds - start.uptimeNanoseconds
+    let timeInterval = Double(nano) / 1_000_000_000
+
+    print("Block duration: \(timeInterval)")
+
+    return returnValue
+}
